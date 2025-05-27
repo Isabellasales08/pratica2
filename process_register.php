d39c41542c35b7775188b319b4f7f1a1180e9905
Isabellasales08 <salesisabella382@gmail.com>
2025-05-27 10:26:06 -0300

    eg

--- process_register.php
+++ process_register.php
@@ -1,38 +1,32 @@
 <?php 
-//Inclui o arquivo de conexão com o banco de dados
+
 require 'config/db.php';
 
-//Verifica se o formulário foi enviado utilizando o método POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //Pega os dados do formulário enviados pelo método POST
-    $nome = $_POST['nome'];     //Nome digitando pelo usuário
-    $email = $_POST['email'];   //Email digitando pelo usauário
+// Verifica se o formulário foi enviado utilizando o método POST
+if ($_SERVER ["REQUEST_METHOD"] == "PODST") {
 
-    //Criptografa a senha antes de salvar no banco de dados
-    //Password_hash() é uma função segura que utiliza o algoritmo bcrypt por padrão
-    $senha  = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+//pega os dados do formulário enviados pelo método POST
+$nome = $_POST [ 'nome'];     //Nome digitado pelo usuário 
+$email = $_POST['email'];     //Email digitado pelo usuário
 
-    //Prepara o query SQL com parâmetros (?) para evitar SQL Injection
-    $stmt = $conn->prepare("INSERT INTO usuarioS (nome, email, senha) VALUES (?, ?, ?)");
+//Criptografia a senha antes de salvar no banco de dados
+// password_hash() é uma função segura que utiliza o algoritimo bcrypt por padrão 
+$senha = password_hash($_POST ['senha'], PASSWORD_DEFAULT);
 
-    //Substitui os parâmetros  da query pelos valeres reais de forma segura
-    //"sss" significa que estamos passando 3 strings (s = string)
-    $stmt->bind_param("sss", $nome, $email, $senha);
+//Prepara a query SQL com paramêtros (?) para evitar SQL Injection
+$stmt = $conn->prepare("INSERT INTO usuário (nome, email, senha) VALUES (?,?,?)");
 
-    //Executa a query SQL no banco de dados
-    if ($stmt->execute()) {
-        //Se o cadastro for feito com sucesso, redireciona para a tela de login
-        //Envia o parâmetro "sucesso=1" na URL para mostrar mensagem de login
-        header("Location: login.php?sucesso=1");
-        exit; //Finaliza o script imediatamente após o redirecionamento
-} else {
-     // Caso ocorra algum erro (ex: email duplicado), exibe o erro na tela 
-    echo "Erro ao cadastrar: " . $conn->error;
- 
+//substitui os parâmetros da query pelos valores reais de forma segura
+//"sss" significa que estamos passando 3 strings ( s = string)
+$stmt ->bind_param ("sss" , $nome, $email, $senha);
 
+// Executa a query SQL no banco de dados
+if ($stmt->execute()) {
+// Se  o cadastro for feito com sucesso, redireciona para a tela de login
+// Envia o parâmetro "sucesso=1" na URL para mostrar mensagem no login
+header("Location: login.php?sucesso=1");
+exit; // Finaliza o script imediatamente após
 }
 
-
 }
-
 ?>
